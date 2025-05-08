bd3b51101385e77c706b011f83359e167506f843
Millovoyman <millovichfm@gmail.com>
2025-05-08 13:56:00 -0600

    Create 5Enteros.c

---
+++ 5Enteros.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int list[5]={57,73,24,12,20};
+    int i;
+    printf("los números de la lista son: ");
+    for (i=0; i<5; i++)
+    {
+        printf("%d, ", list[i]);
+    }
+    printf("\n");
+}
